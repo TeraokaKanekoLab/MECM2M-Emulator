1dfe47a62e46bd1da576a84b9aa1732323086c15
rand16 <rand@inl.ics.keio.ac.jp>
2023-10-29 04:15:28 +0000

    update gitignore

--- Client/main.go
+++ Client/main.go
@@ -16,16 +16,24 @@ import (
 	"os"
 	"strconv"
 	"time"
+
+	"github.com/joho/godotenv"
 )
 
 var (
-	command  *string
-	area_num *int
-	target   *string
+	command    *string
+	area_num   *int
+	target     *string
+	vnode_type *string
+	ad         string
 )
 
 func main() {
-	var ad string
+	// .envファイルの読み込み
+	if err := godotenv.Load(os.Getenv("HOME") + "/.env"); err != nil {
+		log.Fatal(err)
+	}
+
 	var data any
 	var url string
 
@@ -33,9 +41,11 @@ func main() {
 	area_num = flag.Int("area_num", 1, "指定するエリア範囲を選択")
 	target = flag.String("target", "own", "自MEC or 他MEC")
 
+	vnode_type = flag.String("vnode_type", "VSNode", "VNodeの種類")
+
 	flag.Parse()
 
-	data, url = switchM2MAPI(*command, ad)
+	data, url = switchM2MAPI(*command)
 
 	client_data, err := json.Marshal(data)
 	if err != nil {
@@ -53,22 +63,48 @@ func main() {
 	}
 	defer response.Body.Close()
 
-	_, err1 := io.ReadAll(response.Body)
+	body, err1 := io.ReadAll(response.Body)
 	if err != nil {
 		panic(err1)
 	}
 
-	// results := formatBody(args[1], body)
+	switch *command {
+	case "area":
+		results := &m2mapp.ResolveAreaOutput{}
+		if err = json.Unmarshal(body, results); err != nil {
+			fmt.Println("Error unmarshaling: ", err)
+		}
+		path := os.Getenv("HOME") + os.Getenv("PROJECT_NAME") + "/Client/ad.txt"
+		file, _ := os.Create(path)
+		defer file.Close()
+
+		fmt.Fprintf(file, "%s", results.AD)
+
+		//byte_data, _ := json.Marshal(results.AD)
+		//file.Write(byte_data)
+	case "node":
+		results := &m2mapp.ResolveNodeOutput{}
+		if err = json.Unmarshal(body, results); err != nil {
+			fmt.Println("Error unmarshaling: ", err)
+		}
+		path := os.Getenv("HOME") + os.Getenv("PROJECT_NAME") + "/Client/node.csv"
+		file, _ := os.Create(path)
+		defer file.Close()
+
+		for _, i := range results.VNode {
+			fmt.Fprintf(file, "%v,%v,%v\n", i.VNodeID, i.VNodeSocketAddress, i.VMNodeRSocketAddress)
+		}
+	}
 
 	// fmt.Println("Server Response:", results)
 
 	elapsedTime := time.Since(start)
 	durationInNanoseconds := float64(elapsedTime.Nanoseconds())
 	durationInMilliSeconds := durationInNanoseconds / 1e6
-	fmt.Printf("%.3f", durationInMilliSeconds)
+	fmt.Printf("%.3f\n", durationInMilliSeconds)
 }
 
-func switchM2MAPI(command, ad string) (data any, url string) {
+func switchM2MAPI(command string) (data any, url string) {
 	switch command {
 	case "area":
 		var nelat, nelon, swlat, swlon float64
@@ -106,10 +142,16 @@ func switchM2MAPI(command, ad string) (data any, url string) {
 		}
 		url = "http://localhost:8080/m2mapi/area"
 	case "node":
+		file, _ := os.Open("ad.txt")
+		defer file.Close()
+
+		dat, _ := io.ReadAll(file)
+		ad = string(dat)
+		fmt.Println(ad)
 		data = m2mapp.ResolveNodeInput{
 			AD:         ad,
 			Capability: []string{"Temperature", "Humidity", "WindSpeed"},
-			NodeType:   "VSNode",
+			NodeType:   *vnode_type,
 		}
 		url = "http://localhost:8080/m2mapi/node"
 	case "past_node":
@@ -121,10 +163,19 @@ func switchM2MAPI(command, ad string) (data any, url string) {
 		}
 		url = "http://localhost:8080/m2mapi/data/past/node"
 	case "current_node":
+		file, _ := os.Open("node.csv")
+		defer file.Close()
+
+		reader := csv.NewReader(file)
+		rows, _ := reader.ReadAll()
+
+		randomIndex := rand.Intn(len(rows))
+		vnode_id := rows[randomIndex][0]
+		socket_address := rows[randomIndex][1]
 		data = m2mapp.ResolveDataByNodeInput{
-			VNodeID:       "9223372036854775808",
+			VNodeID:       vnode_id,
 			Capability:    []string{"Temperature", "Humidity", "WindSpeed"},
-			SocketAddress: "192.168.1.1:11000",
+			SocketAddress: socket_address,
 		}
 		url = "http://localhost:8080/m2mapi/data/current/node"
 	case "condition_node":
