39205c0dba0bdd02f4f55c6c11f2d03d9b1f3662
rand16 <rand@inl.ics.keio.ac.jp>
2023-11-12 13:00:40 +0000

    update sensingdb

--- setup/SensingDB/make_sql_file.py
+++ setup/SensingDB/make_sql_file.py
@@ -8,10 +8,10 @@ load_dotenv()
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("minute", type=int, help="分")
+parser.add_argument("hour", type=int, help="時")
 args = parser.parse_args()
 
-m = args.minute
+m = args.hour
 
 config_main_psnode_path = os.getenv("PROJECT_PATH") + "/setup/GraphDB/config/config_main_psnode.json"
 with open(config_main_psnode_path, 'r') as file:
@@ -27,22 +27,22 @@ with open(sql_file_path, 'a') as sql_file:
     sql_file.write(insert_command)
 
 psnode_array = psnode_data["psnodes"]
-for minute in range(m, m+1):
-    for second in range(60):
-        if len(str(second)) == 1:
-            second_str = f"0{str(second)}"
+for hour in range(m, m+1):
+    for minute in range(60):
+        if len(str(minute)) == 1:
+            minute_str = f"0{str(minute)}"
         else:
-            second_str = str(second)
+            minute_str = str(minute)
         for i, psnode in enumerate(psnode_array):
             psnode_data_property = psnode["psnode"]["data-property"]
             pnode_id = "\"" + psnode_data_property["PNodeID"] + "\""
             capability = "\"" + psnode_data_property["Capability"] + "\""
-            timestamp = "\"" + f"2023-10-31 10:0{str(minute)}:{second_str} +0900 JST" + "\""
+            timestamp = "\"" + f"2023-10-31 0{str(hour)}:{minute_str}:00 +0900 JST" + "\""
             value = round(random.uniform(30, 40), 3)
             psink_id = "\"PSinkID\""
             lat = psnode_data_property["Position"][0]
             lon = psnode_data_property["Position"][1]
-            if minute == m and second == 59 and i == len(psnode_array)-1:
+            if hour == m and minute == 59 and i == len(psnode_array)-1:
                 insert_line = f"({pnode_id}, {capability}, {timestamp}, {value}, {psink_id}, {lat}, {lon})\n"
             else:
                 insert_line = f"({pnode_id}, {capability}, {timestamp}, {value}, {psink_id}, {lat}, {lon}),\n"
